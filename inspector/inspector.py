2a4f3a8be0a286f1fbb3024fe1c8472f57409c5d
sam <samavard@gmail.com>
2026-03-04 20:58:07 +0000

    Initial commit and set up for the object inspector side project

---
+++ inspector/inspector.py
@@ -0,0 +1,36 @@
+"""Inspects and displays everything knowable about a python object in a readable way.
+
+TODO: More in depth description of what the module does (is it a module)?
+
+TODO: Typical usage example:
+    
+    inspector(object)
+"""
+
+def main():
+    pass
+
+
+def get_identity(obj: Any) -> dict:
+    """    
+    Take any object and return a dictionary of its identity properties.
+
+    Args:
+        obj: Any python object.
+
+    Returns:
+        A dict containing the identity properties of the object.
+
+    Function has no meaningful error cases as it should be callable on any object.
+    """
+    pass
+    # TODO: Get name of obj (is the name passed to the function. Is it always)?
+    # TODO: Get value of obj.
+    # TODO: Get Address of obj.
+    # TODO: Get refcount of obj.
+    # TODO: Get size of obj.
+
+
+if __name__ == "__main__":
+    main()
+
