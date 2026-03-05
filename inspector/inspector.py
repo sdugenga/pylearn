5f147aa0670dd4cb04606b3ed1d290ed3c5f2afd
sam <samavard@gmail.com>
2026-03-05 14:20:19 +0000

    Implemented Problem 1 for the inspector side project including a parametrized test.

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -13,32 +13,21 @@ import sys
 def main():
     pass
 
-
 def get_identity(obj: Any) -> dict:
     """    
     Take any object and return a dictionary of its identity properties.
 
-    Args:
-        obj: Any python object.
-
     Returns:
         A dict containing the identity properties of the object.
-
-    Function has no meaningful error cases as it should be callable on any object.
+            id: the memory address of the object as an integer.
+            ref_count: the number of references to the object.
+            size: the size of the object in bytes.
     """
-    pass
-    # TODO: Get name of obj (is the name passed to the function. Is it always)?
-    # TODO: Get value of obj. 
-    # TODO: Get Address of obj. id(obj)
-    # TODO: Get refcount of obj. sys.getrefcount(obj)
-    # TODO: Get size of obj. sys.getsizeof()
-    # return {
-            # 'name': '#notsure',
-            # 'value': 'obj',
-            # 'id': 'id(obj)',
-            # 'ref_count': 'sys.getrefcount(obj)',
-            # 'size': 'sys.getsizeof(obj)'
-            }
+    return {
+            'id': id(obj),
+            'ref_count': sys.getrefcount(obj),
+            'size': obj.__sizeof__()
+           }
 
 
 if __name__ == "__main__":
---
+++ inspector/test_inspector.py
@@ -0,0 +1,27 @@
+import pytest
+
+from inspector import (
+        get_identity,
+        )
+
+
+def param_add(a, b): # Dummy function to run parametrized tests on functions.
+    return a + b
+
+
+class TestParamGetIdentity:
+
+    @pytest.mark.parametrize("obj", [
+                                        5,
+                                        "Hello, World!",
+                                        3.14,
+                                        [1, 2, 3],
+                                        param_add
+                                     ]
+                              )
+    def test_param_get_identity(self, obj):
+        test_dict = get_identity(obj)
+        assert isinstance(test_dict['id'], int) and test_dict['id'] > 0
+        assert isinstance(test_dict['ref_count'], int) and test_dict['ref_count'] > 0
+        assert isinstance(test_dict['size'], int) and test_dict['size'] > 0
+
