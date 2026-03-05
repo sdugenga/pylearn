76d45a8c3c09b59f46f866f7f62a4cc302c3e494
sam <samavard@gmail.com>
2026-03-05 14:42:09 +0000

    Started implementation of get_type_info

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -30,6 +30,19 @@ def get_identity(obj: Any) -> dict:
            }
 
 
+def get_type_info(obj: Any) -> dict:
+    """
+    Take any object and return a dictionary of it's type properties.
+
+    Returns:
+        A dict containing the type properties of the object:
+            type: The name of type of the object as a string (e.g 'str')
+            mutable: Whether the object type is mutable or not (e.g. 'yes')
+            hierarchy: The type hierarchy of the object as a list from most specific
+            to most general (e.g. [int, object]) # I think!
+    """
+    pass
+
 if __name__ == "__main__":
     main()
 
